2598536a83dd32330d645955c7ec7e169ac37627
lasperilla <l.asperilla@gmail.com>
2017-06-13 00:44:52 -0400

    started on click for comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,38 @@
- alert("hi")
+// alert("hi")
+
+        // $(document).click(function() {
+        //   alert("hi")
+        //   // // Save the id from the p tag
+        //   // var thisId = $(this).attr("data-id");
+        //   // // Empty the comments from the comment section
+        //   // $("#comment-"+thisId+"\"").empty();
+
+        //   // // Now make an ajax call for the Article
+        //   // $.ajax({
+        //   //   method: "GET",
+        //   //   url: "/articles/" + thisId
+        //   // })
+        //   //   // With that done, add the comment information to the page
+        //   //   .done(function(data) {
+        //   //     console.log(data);
+        //   //     // The title of the article
+        //   //     $("#comment-"+thisId+"\"").append("<h2>" + data.title + "</h2>");
+        //   //     // An input to enter a new title
+        //   //     $("#comment-"+thisId+"\"").append("<input id='titleinput' name='title' >");
+        //   //     // A textarea to add a new comment body
+        //   //     $("#comment-"+thisId+"\"").append("<textarea id='bodyinput' name='body'></textarea>");
+        //   //     // A button to submit a new comment, with the id of the article saved to it
+        //   //     $("#comment-"+thisId+"\"").append("<button data-id='" + data._id + "' id='savecomment'>Save Comment</button>");
+
+        //   //     // If there's a comment in the article
+        //   //     if (data.comment) {
+        //   //       // Place the title of the comment in the title input
+        //   //       $("#titleinput").val(data.comment.title);
+        //   //       // Place the body of the comment in the body textarea
+        //   //       $("#bodyinput").val(data.comment.body);
+        //   //     }
+        //   //   });
+        // });
 
 // // Whenever someone clicks a p tag
 // $(document).on("click", function() {
